5a2c1b6e266db2372f26c4a69c6651944cb16584
Marina Yakubovskaya <maribars93@yandex.ru>
2025-10-31 19:00:05 +0300

    chore: add prettier and eslint for code formatting

--- src/scripts/creatUI.js
+++
@@ -1,7 +0,0 @@
-function createContainer(parentId) {
-  const container = document.createElement('div');
-  container.id = 'keyboard-container';
-  container.classList.add('keyboard-container');
-  document.getElementById(parentId).appendChild(container);
-  return container;
-}
