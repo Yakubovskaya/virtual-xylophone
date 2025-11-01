1683564196222b0940429e2edf8ef3ab5bfac184
Marina Yakubovskaya <maribars93@yandex.ru>
2025-11-01 12:45:37 +0300

    fix: delete bootstrap from project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import 'modern-normalize/modern-normalize.css';
 import "../styles/style.scss";
 import { initUI } from "./createUI";
