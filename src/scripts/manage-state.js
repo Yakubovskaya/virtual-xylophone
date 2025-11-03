e307eaa64405e39a5524a949848cf73679fcaa35
Marina Yakubovskaya <maribars93@yandex.ru>
2025-11-04 01:11:21 +0300

    feat: disable edit button during sequence playback

--- src/scripts/manage-state.js
+++ src/scripts/manage-state.js
@@ -4,7 +4,10 @@ const disableInteraction = (button, input, keys) => {
   input.classList.add("disabled");
   button.classList.add("disabled", "button--disabled");
 
-  keys.forEach((key) => key.keyButton.classList.add("disabled"));
+  keys.forEach((key) => {
+    key.keyButton.classList.add("disabled");
+    key.editButton.classList.add("disabled", "button--disabled");
+  });
 };
 
 const enableInteraction = (button, input, keys) => {
@@ -13,7 +16,10 @@ const enableInteraction = (button, input, keys) => {
   input.classList.remove("disabled");
   button.classList.remove("disabled", "button--disabled");
 
-  keys.forEach((key) => key.keyButton.classList.remove("disabled"));
+  keys.forEach((key) => {
+    key.keyButton.classList.remove("disabled");
+    key.editButton.classList.remove("disabled", "button--disabled");
+  });
 };
 
 export { disableInteraction, enableInteraction };
